692f18eecfd25dd1d47a06da497449380f6e26ef
Liangyu <57605643+LiangyuW@users.noreply.github.com>
2020-05-19 22:41:27 -0400

    Create merge_point_of_LinkLists.cpp

---
+++ merge_point_of_LinkLists.cpp
@@ -0,0 +1,19 @@
+int findMergeNode(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+SinglyLinkedListNode *node = head1;
+vector<SinglyLinkedListNode*> nodes;
+while(node != NULL){
+nodes.push_back(node);
+node = node->next;
+}
+
+node = head2;
+while(node != NULL){
+if(find(nodes.begin(), nodes.end(), node) != nodes.end()){
+    return node->data;
+}
+node = node->next;
+}
+
+return -1;
+
+}
