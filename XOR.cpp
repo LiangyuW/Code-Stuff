704ea64265d1fff6c1ecd328b76a2aa0eae4050a
Liangyu <57605643+LiangyuW@users.noreply.github.com>
2020-05-27 22:29:54 -0400

    Update XOR.cpp

--- XOR.cpp
+++ XOR.cpp
@@ -1,5 +1,5 @@
 
-// a^a = 0
+// a^a=0  0^a=a^0=a
 
 int lonelyinteger(vector<int> a) {
 
@@ -8,4 +8,23 @@ for(auto k: a){
     n = n^k;
 }
 return n;
+}
+
+
+// n&1  = n%2
+// n>>1 = n/2
+// 1<<n = 2^n
+long sumXor(long n) {
+
+int zeros = 0;
+
+while(n){
+    if((n&1)==0){
+        zeros++;
+    }
+    n = n>>1;
+}
+
+return 1L << zeros;
+
 }
