294576e05024e2909782c2d61c2376b382b7c0a0
Liangyu <57605643+LiangyuW@users.noreply.github.com>
2020-05-27 22:05:32 -0400

    Update variadics_binary_to_decimal.cpp

--- variadics_binary_to_decimal.cpp
+++ variadics_binary_to_decimal.cpp
@@ -3,6 +3,9 @@ template<bool val>
 int reversed_binary_value(){
 return val;
 }
+
+// x << 1 = x*2
+
 template<bool val1, bool val2, bool... valn>
 int reversed_binary_value(){
     return ((reversed_binary_value<val2, valn...>() << 1) + val1);
